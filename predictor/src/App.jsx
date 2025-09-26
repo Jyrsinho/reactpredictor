daf7a73618206c3a9b793922861f50bb4d497bd8
Jyrsinho <jyrgenhuhtala@gmail.com>
2025-09-26 19:14:44 +0300

    serveri joukkueille

---
+++ predictor/src/App.jsx
@@ -0,0 +1,18 @@
+import {useEffect, useState} from 'react'
+
+function App() {
+    const [allTeams, setAllTeams] = useState([]);
+
+    useEffect(() => {
+
+    })
+
+  return (
+    <div>
+        <Controls></Controls>
+    </div>
+
+  )
+}
+
+export default App
